dc9e5d007843541dc75a29901e95fe77c8ed9543
Alesson Renato Lopes Valenca <alessonrenato@gmail.com>
2017-04-23 20:05:10 -0300

    funcao ler base de dados e prototipos

--- hardk.c
+++ hardk.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+
+#define MAXSTR 100
 
 typedef struct s_dados
 {
@@ -14,6 +17,9 @@ typedef struct s_padrao
     struct s_padrao *prox = NULL;
 } t_padrao;
     
+void readFile(char filename[MAXSTR]);
+void insertListDados(t_dados **head, float insert);
+void insertListPadrao(t_padrao **head, t_dados insert);
 
 int main(void)
 {
@@ -57,6 +63,36 @@ void insertListPadrao(t_padrao **head, t_dados insert)
         head = pl;  
     return;
 }
+
+void readFile(char filename[MAXSTR])
+{
+    FILE *pfile;
+    char ch[MAXSTR], *pchar;
+    float insert;
+    t_dados *head = NULL;
+    t_padrao *database = NULL;
+
+    if((pfile = fopen(filename)) == NULL)
+    {
+        printf("Error, filename\n%s not found!",filename);
+        exit(EXIT_FAILURE);
+    }
+    while( ((fgets(ch,MAXSTR,pfile))!= NULL) && (!feof(pfile)))
+    {
+        while((pchar=strchr(ch,' ')) != NULL)
+        {
+            *pchar = '\0';
+            fscanf(ch,"%f",&insert);
+            insertListDados(&head,insert);
+            ch = pchar + sizeof(char);
+        }
+        insertListPadrao(&database,head);
+        head=NULL;
+    }
+    return;
+}
+
+
 
 
 
