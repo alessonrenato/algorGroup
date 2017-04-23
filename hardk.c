f677e0923b2ec348d697c638147fb8f63b33b98b
Alesson Renato Lopes Valenca <alessonrenato@gmail.com>
2017-04-23 16:59:56 -0300

    insert lists

--- hardk.c
+++ hardk.c
@@ -4,14 +4,14 @@
 
 typedef struct s_dados
 {
-    float dados;
-    int *prox = NULL;
+    float dado;
+    struct s_dados *prox = NULL;
 } t_dados;
 
 typedef struct s_padrao
 {
     t_dados dados;
-    int *prox = NULL;
+    struct s_padrao *prox = NULL;
 } t_padrao;
     
 
@@ -20,7 +20,43 @@ int main(void)
     return EXIT_SUCCESS;
 }
 
+void insertListDados(t_dados **head, float insert)
+{
+    t_dados *pl = *head;
+    t_dados *pl = NULL;
+    while(pl != NULL)
+    {
+        plant = pl;
+        pl = pl->prox;
+    }
+    pl = malloc(sizeof(t_dados));
+    pl->dado = insert;
+    pl->prox = NULL;
+    if(plant != NULL)
+        plant->prox = pl;
+    else
+        head = pl;  
+    return;
+}
 
+void insertListPadrao(t_padrao **head, t_dados insert)
+{
+    t_padrao *pl = *head;
+    t_padrao *pl = NULL;
+    while(pl != NULL)
+    {
+        plant = pl;
+        pl = pl->prox;
+    }
+    pl = malloc(sizeof(t_padrao));
+    pl->dados = insert;
+    pl->prox = NULL;
+    if(plant != NULL)
+        plant->prox = pl;
+    else
+        head = pl;  
+    return;
+}
 
 
 
