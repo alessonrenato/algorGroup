0494933ac8f2e6df43004fd1fbd3be9aa795a258
Alesson Renato Lopes Valenca <alessonrenato@gmail.com>
2017-04-23 16:30:13 -0300

    structs de lista

---
+++ hardk.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct s_dados
+{
+    float dados;
+    int *prox = NULL;
+} t_dados;
+
+typedef struct s_padrao
+{
+    t_dados dados;
+    int *prox = NULL;
+} t_padrao;
+    
+
+int main(void)
+{
+    return EXIT_SUCCESS;
+}
+
+
+
+
+
